f7d26c24366826a61e683e98a10fba183fc74cbb
12ColcloughJ <12colcloughj@lytchett.org.uk>
2018-12-25 22:01:48 +0000

    Re-doing the tings

--- main.go
+++ main.go
@@ -9,49 +9,6 @@ import (
 	"github.com/marcusolsson/tui-go"
 )
 
-var (
-	currentDir string = "/home/josh/"
-	currentFiles = []os.FileInfo{}
-	currentFileNames = []string{}
-	lastIndex int = 0
-)
-
-
-func getFileNames(files []os.FileInfo) []string {
-	var out []string
-	for _, file := range files {
-		out = append(out, file.Name())
-	}
-
-	return out
-}
-
-func changeDir(file os.FileInfo) bool {
-	if file.IsDir() {
-		files, err := ioutil.ReadDir(currentDir+file.Name()+"/")
-		if err != nil { log.Fatal(err) }
-		currentFiles = files
-		currentDir = currentDir+file.Name()+"/"
-		currentFileNames = getFileNames(files)
-	}
-
-	return file.IsDir()
-}
-
-func getBackDir(dir string) string {
-	split := strings.Split(currentDir, "/")
-	return strings.Join(split[:len(split)-2], "/")+"/"
-}
-
-
-func goBackDir() {
-	currentDir = getBackDir(currentDir)
-	files, err := ioutil.ReadDir(currentDir)
-	if err != nil { log.Fatal(err) }
-	currentFiles = files
-	currentFileNames = getFileNames(currentFiles)
-}
-
 
 func main() {
 	t := tui.NewTheme()
@@ -88,34 +45,15 @@ func main() {
 	ui.SetKeybinding("q", func() { ui.Quit() })
 
 	ui.SetKeybinding("h", func() {
-		goBackDir()
-		l.RemoveItems()
-		l.AddItems(currentFileNames...)
-		l.SetSelected(lastIndex)
 	})
 
 	ui.SetKeybinding("l", func() {
-		lastIndex = l.Selected()
-		isDir := changeDir(currentFiles[lastIndex])
-		l.RemoveItems()
-		l.AddItems(currentFileNames...)
-		if isDir {
-			l.SetSelected(0)
-		} else {
-			l.SetSelected(lastIndex)
-		}
 	})
 
 	ui.SetKeybinding("k", func() {
-		if l.Selected() > 0 {
-			l.Select(l.Selected()-1)
-		}
 	})
 
 	ui.SetKeybinding("j", func() {
-		if l.Selected() < len(currentFileNames)-1 {
-			l.Select(l.Selected()+1)
-		}
 	})
 
 	if err := ui.Run(); err != nil {
